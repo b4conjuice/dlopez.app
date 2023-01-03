3c88fc6b116a34d1997ef2fa624705cadd7e6cdc
Daniel Lopez <dlopez807@gmail.com>
2023-01-03 15:48:17 -0800

    fix redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
+const redirects = require('./src/lib/redirects')
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return Object.entries(redirects).map(([key, value]) => ({
+      source: `/${key}`,
+      destination: value,
+      permanent: false,
+    }))
+  },
 }
